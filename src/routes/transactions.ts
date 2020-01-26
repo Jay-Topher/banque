2ce20baee254251ab2431d4e9f06d4bd31f308d1
Jones Ogolo <jay.topher11@gmail.com>
2020-01-27 00:04:02 +0100

    created route to add a transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -24,3 +24,41 @@ router.get('/', async (_req, res) => {
   }
 });
 
+// add transaction
+router.post('/:userId', async (req, res) => {
+  const user = req.params.userId;
+  if (!user) {
+    res.status(400).json({ msg: 'Unable to add' });
+
+    return;
+  }
+  try {
+    const {
+      benefactor,
+      transactionType,
+      transactionAmount,
+      description,
+    } = req.body;
+    const body: ITransaction = {
+      user,
+      benefactor,
+      transactionType,
+      transactionAmount,
+      description,
+    };
+    const doc = await addTransaction(body);
+
+    if (!doc) {
+      res.status(400).json({ msg: 'Could not add transaction' });
+
+      return;
+    }
+
+    res.status(201).json({ doc });
+
+    return;
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
