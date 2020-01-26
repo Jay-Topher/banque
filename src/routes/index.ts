6ee0bbf3c6f99c22f1cdf2d860cc9f5030741373
Jones Ogolo <jay.topher11@gmail.com>
2020-01-26 23:58:47 +0100

    mocked a route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import sampleController from '../controllers/sample';
+// import sampleController from '../controllers/sample';
 
 const router = Router();
 
 router.get('/', function(_req, res, _next) {
-  const message = sampleController();
+  // const message = sampleController();
+  const message = 'Hello World';
 
   res.status(200).json({ message });
 });
