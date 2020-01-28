07c998824e38722da037613eb51f2792c296702b
Jones Ogolo <jay.topher11@gmail.com>
2020-01-28 20:04:27 +0100

    created routes to credit and transfer

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,6 +6,12 @@ import {
   viewTransactionsByUser,
 } from '../controllers/transactions';
 import { ITransaction } from '../../types';
+import {
+  getAccount,
+  debitAccount,
+  creditAccount,
+  checkAccount,
+} from '../controllers/accounts';
 
 const router = Router();
 
@@ -119,6 +125,120 @@ router.get('/:transactionId', async (req, res) => {
     res.status(200).json({ doc });
 
     return;
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+
+    return;
+  }
+});
+
+// credit your account
+router.patch('/:userId/credit', async (req, res) => {
+  const amount = req.body.amount;
+  const userId = req.params.userId;
+
+  try {
+    const accountToCredit = await getAccount(userId);
+
+    if (!accountToCredit) {
+      res.status(404).json({ err: 'Account not found' });
+
+      return;
+    }
+
+    const accountNumber = accountToCredit.accountNumber;
+
+    const credittedAccount = await creditAccount(accountNumber, amount);
+
+    if (!credittedAccount) {
+      res.status(400).json({ err: 'Unable to credit' });
+
+      return;
+    }
+
+    const newTransaction = addTransaction({
+      user: userId,
+      benefactor: userId,
+      transactionType: 'CREDIT',
+      transactionAmount: Number(amount),
+    });
+
+    res.status(200).json({ credittedAccount, newTransaction });
+
+    return;
+  } catch (err) {
+    console.log('failed');
+    res.status(500).json({ err: err.message });
+
+    return;
+  }
+});
+
+// transfer funds from one account to another
+router.patch('/:userId/transfer', async (req, res) => {
+  const { amount, description, accountNumber } = req.body;
+  const userId = req.params.userId;
+
+  try {
+    const [accountToDebit, accountExists] = await Promise.all([
+      getAccount(userId),
+      checkAccount(accountNumber),
+    ]);
+
+    if (!accountToDebit) {
+      res.status(400).json({ msg: 'Invalid Account' });
+
+      return;
+    }
+
+    if (!accountExists) {
+      res.status(401).json({ msg: 'Invalid account number' });
+
+      return;
+    }
+
+    const debittedAccount = await debitAccount(
+      accountToDebit.accountNumber,
+      amount,
+    );
+
+    if (!debittedAccount) {
+      res.status(400).json({ msg: 'Unable to debit' });
+
+      return;
+    }
+
+    const credittedAccount = await creditAccount(accountNumber, amount);
+
+    if (!credittedAccount) {
+      res.status(400).json({ msg: 'Unable to credit' });
+
+      return;
+    }
+
+    const [creditTransaction, debitTransaction] = await Promise.all([
+      addTransaction({
+        user: credittedAccount.user,
+        benefactor: userId,
+        transactionType: 'CREDIT',
+        transactionAmount: Number(amount),
+        description,
+      }),
+      addTransaction({
+        user: userId,
+        benefactor: credittedAccount.user,
+        transactionType: 'DEBIT',
+        transactionAmount: Number(amount),
+        description,
+      }),
+    ]);
+
+    res.status(200).json({
+      debittedAccount,
+      credittedAccount,
+      debitTransaction,
+      creditTransaction,
+    });
   } catch (err) {
     res.status(500).json({ err: err.message });
 
