7351df373cf4ef31a2c8ed59abf8c8852718496f
Jones Ogolo <jay.topher11@gmail.com>
2020-01-29 23:52:07 +0100

    added pin to joi validation and hashed it

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -102,6 +102,11 @@ router.post('/', async (req, res) => {
       .max(50)
       .lowercase()
       .required(),
+    pin: joi
+      .string()
+      .min(4)
+      .max(4)
+      .pattern(/^\d{4}$/),
   });
   const { error, value } = schema.validate(req.body, {
     abortEarly: false,
@@ -115,7 +120,10 @@ router.post('/', async (req, res) => {
 
   try {
     const salt = await bcrypt.genSalt(10);
-    value.password = await bcrypt.hash(value.password, salt);
+    [value.password, value.pin] = await Promise.all([
+      bcrypt.hash(value.password, salt),
+      bcrypt.hash(value.pin, salt),
+    ]);
 
     const doc = await createUser(value);
     const userId = doc.id;
@@ -277,98 +285,5 @@ router.get('/:userId/accounts', async (req, res) => {
     return;
   }
 });
-
-// // credit your account
-// router.patch('/:userId/accounts/:accountNumber', async (req, res) => {
-//   const userId = req.params.userId;
-//   const accountNumber = req.params.accountNumber;
-//   const amount = req.body.amount;
-//   const description = req.body.description;
-
-//   // TODO Card verification
-
-//   try {
-//     const doc = await creditAccount(accountNumber, amount);
-
-//     if (!doc) {
-//       res.status(404).json({ msg: 'Account not found' });
-
-//       return;
-//     }
-//     const body: ITransaction = {
-//       user: userId,
-//       benefactor: userId,
-//       transactionType: 'CREDIT',
-//       description,
-//       transactionAmount: Number(amount),
-//     };
-//     const transaction = await addTransaction(body);
-
-//     res.status(201).json({ doc, transaction });
-
-//     return;
-//   } catch (error) {
-//     res.status(500).json({ msg: error.message });
-
-//     return;
-//   }
-// });
-
-// // transfer funds
-// router.patch(
-//   '/:userId/accounts/:accountNumber/transfer/:receiver',
-//   async (req, res) => {
-//     const { userId, accountNumber, receiver } = req.params;
-//     const { amount, description } = req.body;
-//     // verify accounts
-//     const [account1, account2] = await Promise.all([
-//       checkAccount(accountNumber),
-//       checkAccount(receiver),
-//     ]);
-
-//     if (!account1 || !account2) {
-//       res.status(401).json({ err: 'Invalid accounts' });
-
-//       return;
-//     }
-//     // debit user
-//     try {
-//       const debitted = await debitAccount(accountNumber, amount);
-//       if (!debitted) {
-//         res.status(400).json({ msg: 'Something went wrong' });
-
-//         return;
-//       }
-
-//       const creditted = await creditAccount(receiver, amount);
-//       if (!creditted) {
-//         res.status(400).json({ msg: 'Something went wrong' });
-
-//         return;
-//       }
-
-//       const debitBody: ITransaction = {
-//         user: userId,
-//         benefactor: receiver,
-//         transactionType: 'DEBIT',
-//         transactionAmount: Number(amount),
-//         description,
-//       };
-//       const creditBody: ITransaction = {
-//         user: receiver,
-//         benefactor: userId,
-//         transactionType: 'CREDIT',
-//         transactionAmount: Number(amount),
-//         description,
-//       };
-//       const [transaction1, transaction2] = await Promise.all([
-//         addTransaction(creditBody),
-//         addTransaction(debitBody),
-//       ]);
-
-//       res.status(201).json({ creditted, debitted, transaction1, transaction2 });
-//     } catch (err) {}
-//   },
-// );
 
 export default router;
