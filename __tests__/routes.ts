791e2ce69ef411cdf33ee9e4f118c79718e30e7c
Jones Ogolo <jay.topher11@gmail.com>
2020-01-31 21:27:49 +0100

    added tests

--- __tests__/routes.ts
+++ __tests__/routes.ts
@@ -1,12 +1,55 @@
 import request from 'supertest';
 
 import app from '../src/app';
+import { connect, disconnect } from '../scripts/mongo-setup';
+
+beforeAll(connect);
+afterAll(disconnect);
 
 describe('Server', () => {
   test('Has a /api endpoint', () => {
     return request(app)
-      .get('/api')
+      .get('/')
       .expect('Content-Type', /json/)
-      .expect(200, { message: { hello: 'Hello World' } });
+      .expect(200, { message: 'Hello World' });
   });
+});
+
+describe('Server', () => {
+  test('Has a /api/v1/transactions endpoint', async () => {
+    return await request(app)
+      .get('/api/v1/transactions')
+      .expect('Content-Type', /json/)
+      .expect(200, { doc: [] });
+  });
+});
+
+// describe('Add users', () => {
+//   let data = {
+//     firstName: 'Jones',
+//     lastName: 'Ogolo',
+//     phone: '08044556678',
+//     bvn: '1234567893',
+//     email: 'jones@aol.com',
+//     password: '123abc',
+//     pin: '1234',
+//   };
+//   it('Should create a new user', async done => {
+//     const res = await request(app)
+//       .post('/api/v1/users')
+//       .send(data);
+
+//     expect(res.status).toBe(201);
+//     // expect(res.body).toHaveProperty('doc');
+//     done();
+//   });
+// });
+
+it('Results in an error when querying a non-existent user', async () => {
+  const response = await request(app).get(
+    '/api/v1/users/5e327126aee23c37c4d28eb1',
+  );
+
+  expect(response.status).toBe(404);
+  expect(response.body).toEqual({ message: 'User not found' });
 });
