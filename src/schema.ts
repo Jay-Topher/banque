868017e16e41f323f11658d14d03e247d666bd2c
thaleprechaun <oakinbiyiusman@gmail.com>
2020-02-07 09:21:26 +0100

    update(schema): add query schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,16 +1,53 @@
-import { GraphQLObjectType, GraphQLSchema } from 'graphql';
+import {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLString,
+  GraphQLID,
+} from 'graphql';
 
-// import sampleController from './controllers/sample';
-import SampleType from './types/users';
+import userType from './types/users';
+import { viewAllAccounts, viewAnAccount } from './controllers/accounts';
+import { getAllUsers, getAUser } from './controllers/user';
+import accountType from './types/accounts';
 
 const query = new GraphQLObjectType({
   name: 'Query',
-  description: 'The query root of Nert.',
+  description: 'The query root of Users.',
   fields: () => ({
-    sample: {
-      type: SampleType,
-      description: 'A sample root schema',
-      // resolve: () => sampleController(),
+    users: {
+      type: new GraphQLList(userType),
+      description: 'Get all users details',
+      resolve: () => getAllUsers(),
+    },
+    user: {
+      type: userType,
+      description: 'Get a single user details',
+      args: {
+        userId: { type: GraphQLID },
+      },
+      resolve(_parent, { userId }) {
+        return getAUser(userId);
+      },
+    },
+    accounts: {
+      type: new GraphQLList(accountType),
+      description: 'Get all accounts of users(Admin view only)',
+      resolve: parent => {
+        console.log(parent);
+        return viewAllAccounts();
+      },
+    },
+    account: {
+      type: accountType,
+      description: 'Get a user account detail(Admin view only)',
+      args: {
+        accountId: { type: GraphQLID },
+      },
+      resolve(parent, { accountId }) {
+        console.log(parent);
+        return viewAnAccount(accountId);
+      },
     },
   }),
 });
