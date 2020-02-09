56e4331b04e42f8310f71ed035560100095e49d8
Jones Ogolo <jay.topher11@gmail.com>
2020-02-09 11:41:21 +0100

    created custom types

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="react-scripts" />
+
+import { AxiosRequestConfig } from 'axios';
+
+export interface IRegister {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  bvn: string;
+  password: string;
+  pin: string;
+}
+
+export interface IConfig extends AxiosRequestConfig {
+  header: {
+    'Content-Type': string;
+  };
+}
