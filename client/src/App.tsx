93621e920ec18e5b963c89789e0d14bef4e119d9
Jones Ogolo <jay.topher11@gmail.com>
2020-02-09 11:42:12 +0100

    set up redux provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
+import RegisterCard from './Components/Register/RegisterCard';
+import store from './store';
 
 const App: React.FC = () => {
-  return <div className="App">Hello</div>;
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <RegisterCard />
+        </div>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
