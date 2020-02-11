5cd0569f5eed510b030c0e8f7167cae2abc78e6b
Jones Ogolo <jay.topher11@gmail.com>
2020-02-11 09:17:12 +0100

    created a sidebar menu object

---
+++ client/src/Components/Sidebar/menu.tsx
@@ -0,0 +1,29 @@
+export default {
+  user: [
+    {
+      title: 'Dashboard',
+      link: '/dashboard',
+      icon: '/assets/dashboard.svg',
+    },
+    {
+      title: 'My Account',
+      link: '/account',
+      icon: '/assets/account.svg',
+    },
+    {
+      title: 'Account Statement',
+      link: '/statement',
+      icon: '/assets/account_statement.svg',
+    },
+    {
+      title: 'Transfers',
+      link: '/transfers',
+      icon: '/assets/transfer.svg',
+    },
+    {
+      title: 'Logout',
+      link: '/',
+      icon: '/assets/logout.svg',
+    },
+  ],
+};
