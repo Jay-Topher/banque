e6a2bc7965f3acda9ab9360d03383fce7b7b525a
Jones Ogolo <jay.topher11@gmail.com>
2020-02-14 23:44:18 +0100

    implemented page routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './App.css';
-// import RegisterCard from './Components/Register/RegisterCard';
 import store from './store';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Main from './Components/Main/Main';
 import Usernav from './Components/Usernav/Usernav';
-import Dashboard from './Components/Dashboard/Dashboard';
+// import Dashboard from './Components/Dashboard/Dashboard';
+import SuspenseBoundary from './common/Boundary/SuspenseBoundary';
+import PrivateRoute from './Routing/PrivateRoute';
+import RegisterCard from './Components/Register/RegisterCard';
+
+const Dashboard = lazy(() => import('./Components/Dashboard/Dashboard'));
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <Router>
+        <Route exact path="/register" component={RegisterCard} />
         <div className="App">
           <Sidebar />
-          {/* <RegisterCard /> */}
           <Main>
             <Usernav name="Jones Ogolo" />
-            <Dashboard />
+            <SuspenseBoundary>
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                {/* <Dashboard />
+                </PrivateRoute> */}
+
+                {/*<Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/dashboard" component={Dashboard} /> */}
+              </Switch>
+            </SuspenseBoundary>
           </Main>
         </div>
       </Router>
