3642defebddcc06b41a14e19f2bfa6e423a96d80
Jones Ogolo <jay.topher11@gmail.com>
2020-02-16 16:58:32 +0100

    vreated a view to house the sidebar and main components

---
+++ client/src/views/UserView/UserView.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './UserView.scss';
+import { Route, Switch } from 'react-router';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import Main from '../../Components/Main/Main';
+import Usernav from '../../Components/Usernav/Usernav';
+import Dashboard from '../../Components/Dashboard/Dashboard';
+
+const UserView = () => {
+  return (
+    <div className="UserView">
+      <Sidebar />
+      <Main>
+        <Usernav name="Jones Ogolo" />
+        <Switch>
+          <Route exact path="/user">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Main>
+    </div>
+  );
+};
+
+export default UserView;
