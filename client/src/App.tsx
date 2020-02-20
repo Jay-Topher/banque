a9fec693d899fb9c93622bfabecff27b8ace105a
thaleprechaun <oakinbiyiusman@gmail.com>
2020-02-20 10:05:05 +0100

    update(modify file)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import SuspenseBoundary from './common/Boundary/SuspenseBoundary';
 import PrivateRoute from './Routing/PrivateRoute';
-import Main from './Components/Main/Main';
-import Sidebar from './Components/Sidebar/Sidebar';
-import Usernav from './Components/Usernav/Usernav';
-import TransferView from './views/TransferView/TransferView';
 
 const UserView = lazy(() => import('./views/UserView/UserView'));
 const RegisterCard = lazy(() => import('./Components/Register/RegisterCard'));
@@ -15,21 +11,13 @@ const LoginCard = lazy(() => import('./Components/Login/LoginCard'));
 const App = () => {
   return (
     <Router>
-      {/* <SuspenseBoundary>
+      <SuspenseBoundary>
         <Switch>
           <Route exact path="/register" component={RegisterCard} />
           <Route path="/login" component={LoginCard} />
-          <PrivateRoute exact path="/user" component={UserView} />
+          <PrivateRoute path="/dashboard" component={UserView} />
         </Switch>
-      </SuspenseBoundary> */}
-      {/* /////////////////////////////development//////////////////////////////////////// */}
-      <div className="App">
-        <Sidebar />
-        <Main>
-          <Usernav />
-          <TransferView />
-        </Main>
-      </div>
+      </SuspenseBoundary>
     </Router>
   );
 };
