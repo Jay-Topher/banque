38dd64266e77e896a33e4cef354c25f7bb576008
Jones Ogolo <jay.topher11@gmail.com>
2020-02-20 11:26:02 +0100

    added suspense boundary

--- client/src/views/UserView/UserView.tsx
+++ client/src/views/UserView/UserView.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import './UserView.scss';
 import { Route, Switch } from 'react-router';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import Main from '../../Components/Main/Main';
 import Usernav from '../../Components/Usernav/Usernav';
-import Dashboard from '../../Components/Dashboard/Dashboard';
+import SuspenseBoundary from '../../common/Boundary/SuspenseBoundary';
+// import Dashboard from '../../Components/Dashboard/Dashboard';
+
+const Dashboard = lazy(() => import('../../Components/Dashboard/Dashboard'));
+const TransferView = lazy(() => import('../TransferView/TransferView'));
 
 const UserView = () => {
   return (
-    <div className="UserView">
-      <Sidebar />
-      <Main>
-        <Usernav name="Jones Ogolo" />
-        <Switch>
-          <Route exact path="/user">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </Main>
-    </div>
+    <SuspenseBoundary>
+      <div className="UserView">
+        <Sidebar />
+        <Main>
+          <Usernav name="Jones Ogolo" />
+          <Switch>
+            <Route exact path="/user/dashboard">
+              <Dashboard />
+            </Route>
+            <Route exact path="user/transfers">
+              <TransferView />
+            </Route>
+          </Switch>
+        </Main>
+      </div>
+    </SuspenseBoundary>
   );
 };
 
