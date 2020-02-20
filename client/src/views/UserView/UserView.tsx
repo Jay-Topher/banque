49c9788d2d0ad184147ce1cff9b752618b7308b8
thaleprechaun <oakinbiyiusman@gmail.com>
2020-02-20 09:45:19 +0100

    update(modify file): Add suspense boundary and route component properly

--- client/src/views/UserView/UserView.tsx
+++ client/src/views/UserView/UserView.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './UserView.scss';
 import { Route, Switch } from 'react-router';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import Main from '../../Components/Main/Main';
 import Usernav from '../../Components/Usernav/Usernav';
 import Dashboard from '../../Components/Dashboard/Dashboard';
+import AccountView from '../AccountView/AccountView';
+import SuspenseBoundary from '../../common/Boundary/SuspenseBoundary';
+import TransferView from '../TransferView/TransferView';
 
 const UserView = () => {
   return (
@@ -12,11 +15,19 @@ const UserView = () => {
       <Sidebar />
       <Main>
         <Usernav />
-        <Switch>
-          <Route exact path="/user">
-            <Dashboard />
-          </Route>
-        </Switch>
+        <SuspenseBoundary>
+          <Switch>
+            <Route exact path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route exact path="/dashboard/account">
+              <AccountView />
+            </Route>
+            <Route exact path="/dashboard/transfers">
+              <TransferView />
+            </Route>
+          </Switch>
+        </SuspenseBoundary>
       </Main>
     </div>
   );
