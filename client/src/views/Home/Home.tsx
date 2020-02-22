917dbe3de7fa3344648e90b52dc9558277e73750
Jones Ogolo <jay.topher11@gmail.com>
2020-02-22 17:17:28 +0100

    mounted first section on home view

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
+import FirstSection from '../../Components/FirstSection/FirstSection';
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <FirstSection />
     </div>
   );
 };
