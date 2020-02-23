67443cc496e6025b25b4397188ba97f55c7826c9
Jones Ogolo <jay.topher11@gmail.com>
2020-02-23 15:48:40 +0100

    mounted the second section in the home view

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
 import FirstSection from '../../Components/FirstSection/FirstSection';
+import SecondSection from '../../Components/SecondSection/SecondSection';
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <FirstSection />
+      <SecondSection />
     </div>
   );
 };
